6f16f20ad5c43201f0072abf1bc30ac453a51556
ArturoPF <arturo.pfrz@gmail.com>
2023-10-28 15:24:47 +0200

    Modif y add
    Modificacion para móviles, añadido footer y div precios en index

--- pages/acceso.php
+++ pages/acceso.php
@@ -2,7 +2,7 @@
 session_start();
 require('./conexion.php');
 
-if ($_SESSION['user']) {
+if (isset($_SESSION['user'])) {
   header('Location: datosCliente.php');
 } else {
   if (isset($_POST['user'])) {
@@ -24,8 +24,12 @@ if ($_SESSION['user']) {
     $passObtenida = $exec->fetch(PDO::FETCH_OBJ);
 
     if ($passObtenida->pass == $pass) {
+      if (isset($_SESSION['credencialesErroneas']) && $_SESSION['credencialesErroneas'] == True) $_SESSION['credencialesErroneas'] = False;
       $_SESSION['user'] = $email;
       header('Location: datosCliente.php');
+    } else {
+      $_SESSION['credencialesErroneas'] = True;
+      header('Location: acceso.php');
     }
   } else {
 ?>
@@ -39,14 +43,20 @@ if ($_SESSION['user']) {
       <link rel="stylesheet" type="text/css" href="../css/all.css">
       <link rel="stylesheet" type="text/css" href="../css/menu.css">
       <link rel="stylesheet" type="text/css" href="../css/log.css">
+      <link rel="stylesheet" type="text/css" href="../css/footer.css">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+      <script src="https://kit.fontawesome.com/2eff857ffa.js" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
     </head>
 
     <body>
-      <nav id="navbar">
+      <!-- Menú de pc... -->
+      <nav id="menuPc">
         <div id="menu">
           <div id="logo">
-            <a href="../index.php">
+            <a href="/Proyecto/">
               <h2 class="w700">Gym<span class="naranja">Art</span></h2>
             </a>
           </div>
@@ -57,28 +67,101 @@ if ($_SESSION['user']) {
               <li><a href="#">Valoraciones</a></li>
               <li><a href="#">Miembros</a></li>
               <li><a href="#">Contacto</a></li>
-              <li><a href="./acceso.php" class="naranja">Clientes</a></li>
             </ul>
+          </div>
+          <div id="acceso">
+            <a href="./pages/acceso.php" class="naranja">
+              <h5><i class="bi bi-person-circle"></i></h5>&nbsp
+              <p>
+                <?php
+                if (isset($_SESSION['nombre']))  echo $_SESSION['nombre'];
+                else echo 'Inicia Sesión';
+                ?>
+              </p>
+            </a>
           </div>
         </div>
       </nav>
+      <!-- Menú de móvil -->
+      <nav id="menuMovil" class="navbar navbar-dark bg-dark fixed-top">
+        <div class="container-fluid">
+          <a class="navbar-brand w700" href="/Proyecto/">Gym<span class="naranja">Art</span></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">
+                <a href="./pages/acceso.php"><i class="bi bi-person-circle"></i>
+                  <?php
+                  if (isset($_SESSION['nombre']))  echo $_SESSION['nombre'];
+                  else echo 'Inicia Sesión';
+                  ?>
+                </a>
+              </h5>
+              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                  <a class="nav-link text-center" aria-current="page" href="/Proyecto/">Inicio</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-center" href="#">Galería</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-center" href="#">Valoraciones</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-center" href="#">Miembros</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-center" href="#">Contacto</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active text-center naranja" href="../pages/acceso.php">Acceder</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+
       <div id="img"></div>
       <div id="formAcceso">
         <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
           <fieldset>
             <label for="user">Email:</label><br>
             <div class="input-group mb-3">
-              <input id="user" name="user" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+              <input required id="user" name="user" type="email" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
             </div>
             <label for="pass">Contraseña:</label><br>
             <div class="input-group mb-3">
-              <input id="pass" name="pass" type="password" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+              <input required id="pass" name="pass" type="password" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
             </div>
           </fieldset>
-          <div id="boton"><button type="submit" class="btn btn-primary">Acceder</button></div>
+          <div id="divBoton"><button id="boton" type="submit" class="btn btn-primary">Acceder</button></div>
         </form>
+        <?php if (isset($_SESSION['credencialesErroneas']) && $_SESSION['credencialesErroneas'] == True) {
+          echo '<p style="color:red;width:max-content;margin:20px auto;">Credenciales incorrectas.</p>';
+        } ?>
       </div>
-  <?php }} ?>
+      <footer>
+        <div id="redes">
+          <a href="#"><i class="fa-brands fa-square-instagram fa-2xl"></i></a>
+          <a href="#"><i class="fa-brands fa-square-facebook fa-2xl"></i></a>
+          <a href="#"><i class="fa-brands fa-square-x-twitter fa-2xl"></i></a>
+        </div>
+        <div id="infoFooter">
+          <h3>GymArt</h3>
+          <p>988 123 456</p>
+          <p>info@ejemplocorreo.com</p>
+          <p>Calle de la calle, 22 - 32003 Ourense</p>
+          <a href="#">Aviso Legal</a>
+        </div>
+      </footer>
+  <?php }
+} ?>
     </body>
 
     </html>
--- pages/datosCliente.php
+++ pages/datosCliente.php
@@ -16,6 +16,7 @@ try {
 }
 
 $datos = $exec->fetch(PDO::FETCH_OBJ);
+$_SESSION['nombre'] = $datos->nombre . ' ' . $datos->apellido1 . ' ' . $datos->apellido2;
 
 ?>
 
@@ -28,17 +29,21 @@ $datos = $exec->fetch(PDO::FETCH_OBJ);
     <link rel="stylesheet" type="text/css" href="../css/all.css">
     <link rel="stylesheet" type="text/css" href="../css/menu.css">
     <link rel="stylesheet" type="text/css" href="../css/log.css">
-
+    <link rel="stylesheet" type="text/css" href="../css/footer.css">
     <link rel="stylesheet" type="text/css" href="../css/datosCliente.css">
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <script src="https://kit.fontawesome.com/2eff857ffa.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
 </head>
 
 <body>
-    <nav id="navbar">
+    <!-- Menú de pc... -->
+    <nav id="menuPc">
         <div id="menu">
             <div id="logo">
-                <a href="../index.php">
+                <a href="/Proyecto/">
                     <h2 class="w700">Gym<span class="naranja">Art</span></h2>
                 </a>
             </div>
@@ -49,11 +54,66 @@ $datos = $exec->fetch(PDO::FETCH_OBJ);
                     <li><a href="#">Valoraciones</a></li>
                     <li><a href="#">Miembros</a></li>
                     <li><a href="#">Contacto</a></li>
-                    <li><a href="./acceso.php" class="naranja">Clientes</a></li>
                 </ul>
+            </div>
+            <div id="acceso">
+                <a href="./pages/acceso.php" class="naranja">
+                    <h5><i class="bi bi-person-circle"></i></h5>&nbsp
+                    <p>
+                        <?php
+                        if (isset($_SESSION['nombre']))  echo $_SESSION['nombre'];
+                        else echo 'Inicia Sesión';
+                        ?>
+                    </p>
+                </a>
             </div>
         </div>
     </nav>
+    <!-- Menú de móvil -->
+    <nav id="menuMovil" class="navbar navbar-dark bg-dark fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand w700" href="/Proyecto/">Gym<span class="naranja">Art</span></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">
+                        <a href="./pages/acceso.php"><i class="bi bi-person-circle"></i>
+                            <?php
+                            if (isset($_SESSION['nombre']))  echo $_SESSION['nombre'];
+                            else echo 'Inicia Sesión';
+                            ?>
+                        </a>
+                    </h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link text-center" aria-current="page" href="/Proyecto/">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-center" href="#">Galería</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-center" href="#">Valoraciones</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-center" href="#">Miembros</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-center" href="#">Contacto</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active text-center naranja" href="../pages/acceso.php">Acceder</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+
     <div id="img"></div>
     <div id="divDatos">
         <div>
@@ -64,7 +124,7 @@ $datos = $exec->fetch(PDO::FETCH_OBJ);
             </div>
             <div id="datos2" class="row justify-content-center">
                 <div id="dni" class="col"><strong>DNI:&nbsp;</strong><?php echo "{$datos->dni}"; ?></div>
-                <div id="email" class="col"><strong>Email:&nbsp;</strong><?php echo "{$datos->email}"; ?></div>
+                <div id="email" class="col"><strong>Email:&nbsp;</strong><?php echo " {$datos->email}"; ?></div>
                 <div id="telefono" class="col"><strong>Teléfono:&nbsp;</strong><?php echo "{$datos->telefono}"; ?></div>
             </div>
             <div id="datos3" class="row justify-content-center">
@@ -73,9 +133,24 @@ $datos = $exec->fetch(PDO::FETCH_OBJ);
             <div id="datos4" class="row justify-content-center">
                 <div id="suscripcion" class="col"><strong>Estado suscripción:&nbsp;</strong><?php echo "{$datos->suscripcion}"; ?></div>
             </div>
-            <div id="boton"><button type="submit"><a href="cerrarSesion.php">Cerrar Sesión</a></button></div>
+            <div id="divBoton"><button id="boton" type="submit"><a href="cerrarSesion.php">Cerrar Sesión</a></button></div>
         </div>
     </div>
+
+    <footer>
+        <div id="redes">
+            <a href="#"><i class="fa-brands fa-square-instagram fa-2xl"></i></a>
+            <a href="#"><i class="fa-brands fa-square-facebook fa-2xl"></i></a>
+            <a href="#"><i class="fa-brands fa-square-x-twitter fa-2xl"></i></a>
+        </div>
+        <div id="infoFooter">
+            <h3>GymArt</h3>
+            <p>988 123 456</p>
+            <p>info@ejemplocorreo.com</p>
+            <p>Calle de la calle, 22 - 32003 Ourense</p>
+            <a href="#">Aviso Legal</a>
+        </div>
+    </footer>
 </body>
 
 </html>
